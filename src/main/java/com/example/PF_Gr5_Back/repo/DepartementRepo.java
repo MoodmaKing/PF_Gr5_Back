0577a307a4e8ad0d5da52f83c0077a0034a569e0
elicou <eliz.cou@gmail.com>
2022-01-13 16:50:44 +0100

    change rest call

---
+++ src/main/java/com/example/PF_Gr5_Back/repo/DepartementRepo.java
@@ -0,0 +1,11 @@
+package com.example.PF_Gr5_Back.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.PF_Gr5_Back.model.Departement;
+
+
+
+public interface DepartementRepo extends JpaRepository<Departement, Integer>{
+
+}
